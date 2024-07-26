582a5d143b2f24dfab96c6f309826be89d24effb
GunaNeelamegam <gunag5127@gmail.com>
2024-07-26 17:31:57 +0400

    Interview Code

---
+++ AppConstants.js
@@ -0,0 +1,10 @@
+const AppWidth = 300
+const AppHeight = 600
+const ConnectionURI = "ws://localhost:8765/"
+
+
+
+function connect() {
+    websocket_client.active =true
+    console.log("CONNECTION ESTABLISHED");
+}
